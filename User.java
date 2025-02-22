5a31f22f22c8f40fce804736eabf8aed01b0ab04
anshbkeai <ansh.badam@gmail.com>
2025-02-22 15:25:55 +0530

    Update User.java

--- User.java
+++ User.java
@@ -16,3 +16,92 @@ public class User {
     private List<String> attendedEvents; 
     private List<String> createdEvents;  
 }
+/*
+
+Endpoints
+
+User Management
+
+HTTP Method
+
+Endpoint
+
+Description
+
+GET
+
+/
+
+Get all users
+
+GET
+
+/{id}
+
+Get a user by ID
+
+POST
+
+/
+
+Create a new user
+
+PUT
+
+/{id}
+
+Update user details
+
+DELETE
+
+/{id}
+
+Delete a user
+
+Fetch User-Related Details
+
+HTTP Method
+
+Endpoint
+
+Description
+
+GET
+
+/{id}/projects
+
+Get all projects of a user
+
+GET
+
+/{id}/events/attended
+
+Get all attended events
+
+GET
+
+/{id}/events/created
+
+Get all created events
+
+GET
+
+/{id}/experience
+
+Get work experience
+
+GET
+
+/{id}/education
+
+Get education details
+
+GET
+
+/{id}/links
+
+Get LinkedIn and other links
+
+
+
+*/
